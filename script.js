c893bb18205d637a12e51322584faa401bfb7f54
yugmani <yugmani@yahoo.com>
2021-06-08 12:45:26 -0700

    Calculate average of numbers in array using reduce;

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const reverseString = str =>
 
 // console.log(reverseString("Welcome to JavaScript"));
 // Expected: tpircSavaJ ot emocleW
+
+// 2. Get the Average of an Array of Number
+
+// const averageArray = arr => {
+//   const average = arr.reduce((current, total) => {
+//     return total + current;
+//   }, 0);
+//   return average / arr.length;
+// };
+
+// simplified
+const averageArray = arr => arr.reduce((a, b) => a + b) / arr.length;
+
+const myArray = [21, 56, 23, 122, 67];
+
+console.log(averageArray(myArray));
+// Expected: 57.8
